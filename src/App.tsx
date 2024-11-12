83ed0ba121c564e1114859beebae8913b2e8faf8
Enes Coşkun <enescoskun7777@hotmail.com>
2024-11-12 20:44:11 +0300

    adding analytics from vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Dashboard />
       </main>
+      <Analytics />
     </div>
   );
 }
